f4c72f3f909591de6f5a67250bdb262cd3d67d58
Giovanni Vasconcelos <giovannivmedeiros@gmail.com>
2023-05-18 22:32:20 -0300

    feat: make header component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,17 @@
 <template>
-  <RouterView />
+  <div
+  id="wrapper"
+  class="col-12 p-2">
+    <page-header
+      title="Product List"
+      blueButtonText="Add"
+      redButtonText="Mass delete"
+    />
+  </div>
+<!-- <RouterView /> -->
 </template>
 
 <script>
-import { RouterLink, RouterView } from 'vue-router'
+// import { RouterLink, RouterView } from 'vue-router';
 
 </script>
---
+++ client/src/components/PageHeader.vue
@@ -0,0 +1,47 @@
+<template>
+    <header class="col-12 d-flex align-items-center">
+        <div
+        :class="[
+            sidesClasses,
+            'justify-content-start'
+            ]">
+            <h1>{{ title }}</h1>
+        </div>
+        <div
+            :class="[
+                sidesClasses,
+                'justify-content-end'
+            ]">
+            <a
+            class="btn btn-outline-primary">
+                {{ blueButtonText }}
+            </a>
+            <a
+                class="btn btn-outline-danger"
+                id="mass-delete-btn">
+                    {{ redButtonText }}
+            </a>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                sidesClasses: 'col-6 d-flex gap-2'
+            }
+        },
+        props: {
+            title: {
+                required: 'true'
+            },
+            blueButtonText: {
+                required: 'true'
+            },
+            redButtonText: {
+            required: 'true'
+            }
+        }
+    }
+</script>
