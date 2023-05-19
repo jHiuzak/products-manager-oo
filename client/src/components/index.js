5392b3aa92a47b65036301e2370c3d78b2cf3813
Giovanni Vasconcelos <giovannivmedeiros@gmail.com>
2023-05-18 22:04:26 -0300

    feat: import header and footer in main.js

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+import PageFooter from './PageFooter.vue';
+import PageHeader from './PageHeader.vue';
+
+export {
+    PageFooter,
+    PageHeader
+};
--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,17 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
+import {
+    // PageFooter,
+    PageHeader
+} from './components';
 
 const app = createApp(App)
 
-app.use(router)
+app.use(router);
+app.component('page-header', PageHeader);
 
 app.mount('#app')
