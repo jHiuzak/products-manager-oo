226a69aa06bbb0cb9531ff7ebca225129cc34a71
Giovanni Vasconcelos <giovannivmedeiros@gmail.com>
2023-05-20 15:48:46 -0300

    feat(form input): create basic component for form inputs

---
+++ client/src/components/ProductAdd/FormInput.vue
@@ -0,0 +1,45 @@
+<template>
+  <div :class="labelWrapperClass">
+    <label
+      :for="inputNameAndId"
+      :class="labelClass"
+    >
+      <slot></slot>
+    </label>
+    <div :class="inputWrapperClass">
+      <input
+        :id="inputNameAndId"
+        v-model="inputValue"
+        required="true"
+        :type="inputHtmlType"
+        :class="inputClass"
+        :name="inputNameAndId"
+      />
+    </div>
+    <h6>{{ inputValue }}</h6>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    inputNameAndId: {
+      type: String,
+      required: true,
+    },
+    inputHtmlType: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      labelWrapperClass: 'row mb-3',
+      labelClass: 'col-2 col-form-label fw-bold',
+      inputWrapperClass: 'col-4',
+      inputClass: 'form-control',
+      inputValue: '',
+    };
+  },
+};
+</script>
---
+++ client/src/views/AddProductView.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <page-header
+      title="Product Add"
+      blue-button-text="Save"
+      red-button-text="Cancel"
+    />
+  </header>
+
+  <main>
+    <form
+      id="productForm"
+      method="POST"
+      action="#"
+      class="col-12"
+    >
+      <form-input
+        input-name-and-id="sku"
+        input-html-type="text"
+        >SKU</form-input
+      >
+      <form-input
+        input-name-and-id="price"
+        input-html-type="number"
+        >Price</form-input
+      >
+    </form>
+  </main>
+</template>
+
+<script>
+import { FormInput } from '../components/ProductAdd/';
+
+export default {
+  components: {
+    FormInput,
+  },
+};
+</script>
