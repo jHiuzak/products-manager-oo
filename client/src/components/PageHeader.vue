eadc712691e9ae7ff14967ba7812fdf81af14064
Giovanni Vasconcelos <giovannivmedeiros@gmail.com>
2023-05-20 16:00:04 -0300

    style(page header): format the HTML with attribute per line

--- client/src/components/PageHeader.vue
+++ client/src/components/PageHeader.vue
@@ -1,47 +1,42 @@
 <template>
-    <header class="col-12 d-flex align-items-center">
-        <div
-        :class="[
-            sidesClasses,
-            'justify-content-start'
-            ]">
-            <h1>{{ title }}</h1>
-        </div>
-        <div
-            :class="[
-                sidesClasses,
-                'justify-content-end'
-            ]">
-            <a
-            class="btn btn-outline-primary">
-                {{ blueButtonText }}
-            </a>
-            <a
-                class="btn btn-outline-danger"
-                id="mass-delete-btn">
-                    {{ redButtonText }}
-            </a>
-        </div>
-    </header>
+  <header class="col-12 d-flex align-items-center">
+    <div :class="[sidesClasses, 'justify-content-start']">
+      <h1>{{ title }}</h1>
+    </div>
+    <div :class="[sidesClasses, 'justify-content-end']">
+      <a class="btn btn-outline-primary">
+        {{ blueButtonText }}
+      </a>
+      <a
+        id="mass-delete-btn"
+        class="btn btn-outline-danger"
+      >
+        {{ redButtonText }}
+      </a>
+    </div>
+  </header>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                sidesClasses: 'col-6 d-flex gap-2'
-            }
-        },
-        props: {
-            title: {
-                required: 'true'
-            },
-            blueButtonText: {
-                required: 'true'
-            },
-            redButtonText: {
-            required: 'true'
-            }
-        }
-    }
+export default {
+  props: {
+    title: {
+      type: String,
+      required: 'true',
+    },
+    blueButtonText: {
+      type: String,
+      required: 'true',
+    },
+    redButtonText: {
+      type: String,
+      required: 'true',
+    },
+  },
+  data() {
+    return {
+      sidesClasses: 'col-6 d-flex gap-2',
+    };
+  },
+};
 </script>
