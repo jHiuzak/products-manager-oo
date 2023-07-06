5be5b28aabfbcc75dd91d8bd30d37960c56524c8
Giovanni Vasconcelos <giovannivmedeiros@gmail.com>
2023-07-06 17:02:20 -0300

    feat: create model for product cards

---
+++ client/src/components/Home/ProductCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <label
+    class="product-card d-block form-check-label h-11 w-16 text-center border border-2 border-secondary rounded p-2"
+    for="inlineCheckbox1"
+  >
+    <input
+      id="inlineCheckbox1"
+      class="form-check-input form-check"
+      type="checkbox"
+      value="option1"
+    />
+    <ul class="list-unstyled m-0 d-flex flex-column">
+      <li>JHHJK98430</li>
+      <li>War and Peace</li>
+      <li>$76</li>
+      <li>Weight: 2 KG</li>
+    </ul>
+  </label>
+</template>
+
+<style scoped>
+.product-card {
+  height: 11rem;
+  width: 16rem;
+}
+</style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,8 +1,27 @@
 <template>
-    <page-header
-        title="Product List"
-        blueButtonText="Add"
-        redButtonText="Mass delete"
-    />
-
+  <page-header
+    title="Product List"
+    blue-button-text="Add"
+    red-button-text="Mass delete"
+  />
+  <main class="d-flex justify-content-center flex-wrap gap-3">
+    <product-card />
+    <product-card />
+    <product-card />
+    <product-card />
+    <product-card />
+    <product-card />
+    <product-card />
+    <product-card />
+  </main>
 </template>
+
+<script>
+import { ProductCard } from '../components/Home';
+
+export default {
+  components: {
+    ProductCard,
+  },
+};
+</script>
